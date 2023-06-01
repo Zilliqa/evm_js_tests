29fd2f91310dac336a16c2a6fdce1d0c5177e6aa
n-hutton <nathan.cameron.hutton@gmail.com>
2023-06-02 00:38:55 +0100

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -65,6 +65,21 @@ const config: HardhatUserConfig = {
       zilliqaNetwork: true,
       miningState: false
     },
+    zq2: {
+      url: "http://localhost:4201/",
+      websocketUrl: "ws://localhost:4201/",
+      accounts: [
+        "65d7f4da9bedc8fb79cbf6722342960bbdfb9759bc0d9e3fb4989e831ccbc227",
+        "62070b1a3b5b30236e43b4f1bfd617e1af7474635558314d46127a708b9d302e",
+        "56d7a450d75c6ba2706ef71da6ca80143ec4971add9c44d7d129a12fa7d3a364",
+        "db670cbff28f4b15297d03fafdab8f5303d68b7591bd59e31eaef215dd0f246a"
+      ],
+      chainId: 0x8001,
+      web3ClientVersion: "Zilliqa/v8.2",
+      protocolVersion: 0x41,
+      zilliqaNetwork: true,
+      miningState: false
+    },
     ganache: {
       url: "http://localhost:7545",
       websocketUrl: "ws://localhost:7545",
@@ -197,3 +212,8 @@ task("test")
   });
 
 export default config;
+
+
+
+
+
